e20bfa93668a4ca6bd7e1d1b336f99e6c93ab570
Emerick Rogul <erogul@brave.com>
2019-03-07 14:11:35 -0500

    Expose StartAutoContribute function for testing purposes

--- vendor/bat-native-ledger/src/bat/ledger/internal/ledger_impl.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/ledger_impl.cc
@@ -1246,6 +1246,10 @@ void LedgerImpl::GetRewardsInternalsInfo(ledger::RewardsInternalsInfo* info) {
   }
 }
 
+void LedgerImpl::StartAutoContribute() {
+  bat_contribution_->StartAutoContribute();
+}
+
 const braveledger_bat_helper::WALLET_PROPERTIES_ST&
 LedgerImpl::GetWalletProperties() const {
   return bat_state_->GetWalletProperties();
